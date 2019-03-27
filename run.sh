644d3ca994ac96e1be0ab17379739add3ca55990
Julius Paffrath <juliuspaffrath@web.de>
2019-03-27 10:45:54 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,18 @@ if [ ! -f /home/openplc/scripts/script.st ]; then
     exit 1
 fi
 
-cp /home/openplc/scripts/script.st /home/openplc/OpenPLC_v3/webserver/st_files
+if [ ! -f /home/openplc/scripts/mbconfig.cfg ]; then
+    echo "Config file missing!"
+    exit 1
+fi
 
-sqlite3 /home/openplc/OpenPLC_v3/webserver/openplc.db 'INSERT INTO Programs (Name, Description, File, Date_upload) VALUES ("Test", "Desc", "script.st", strftime("%s", "now"));'
+SQL_SCRIPT="INSERT INTO Programs (Name, Description, File, Date_upload) VALUES ('Test', 'Desc', 'script.st', strftime('%s', 'now'));"
+SQL_DEVICE="INSERT INTO Slave_dev (dev_name, dev_type, slave_id, ip_address, ip_port, di_start, di_size, coil_start, coil_size, ir_start, ir_size, hr_read_start, hr_read_size, hr_write_start, hr_write_size) VALUES ('Testdevice', 'TCP', 0, '127.0.0.1', 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);"
+
+cp /home/openplc/scripts/script.st /home/openplc/OpenPLC_v3/webserver/st_files
+sqlite3 /home/openplc/OpenPLC_v3/webserver/openplc.db "$SQL_SCRIPT"
+
+cp /home/openplc/scripts/mbconfig.cfg /home/openplc/OpenPLC_v3/webserver
+sqlite3 /home/openplc/OpenPLC_v3/webserver/openplc.db "$SQL_DEVICE"
 
 /home/openplc/OpenPLC_v3/start_openplc.sh
